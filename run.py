c76eb63dab2ba3853e4ff9faff072d8b3967eb29
CGPlusPlus <conorplusg@gmail.com>
2022-01-20 23:19:28 +0000

    Added logic for request_player_name function

--- run.py
+++ run.py
@@ -21,6 +21,7 @@ import time
         Player loses
     8. Player granted choice to replay game.
 """
+
 # Generating global variables
 correct_letters = []
 incorrect_letters = []
@@ -32,54 +33,77 @@ game_over = False
 # Game logic functions
 
 
+# Request player to enter name 
 def request_player_name():
-    """Ask player to enter thier Player name"""
+    global player_name
+    
+    # Input field for Player name, strip any whitespace
+    player_name = input('Welcome to Hangman, please enter your Player name: ')
+    player_name = player_name.strip()
+
+    return player_name
 
 
+# Generate a randomly selected word from our predefined list of words
 def select_random_word():
-    """Generate a randomly selected word from our predefined list of words"""
+    pass
 
 
+# Print out the word with dashes for letters that haven't been guessed
 def display_blank_word():
-    """Print out the word with dashes for letters that haven't been guessed yet"""
+    pass
 
 
+# Print out the hangman drawing based on the number of lives left
 def print_hangman():
-    """Print out the hangman drawing based on the number of lives left"""
+    pass
 
 
+# Will make sure the user types only 1 letter that has not been used before
 def letter_validation():
-    """Will make sure the user types only 1 letter that has not been used before"""
+    pass
 
+
+# Check if the letter guess is correct and update variables based on result
 def letter_check_correct():
-    """Will check if the letter guessed is correct and update global variables based on the result"""
+    pass
 
 
+# Check to see if the player has won or lost
 def check_for_game_over():
-    """Check to see if the player has won or lost"""
+    pass
 
 
+# Check is Player wishes to replay, else exit
 def check_for_replay():
-    """Check is Player wishes to replay, else exit"""
+    pass
 
 
+# Game entry point, will call all other methods in a loop
 def main():
-    """Game entry point, will call all other methods in a loop"""
     global game_over
-
+    
     print(" ----- Welcome to Hangman ---- ")
     request_player_name()
     select_random_word()
-
+    
+    # Start while loop for game to run while game_over = False
     while game_over is False:
         print_hangman()
         display_blank_word()
 
+        # Print incorrect guesses before each guess
         if len(incorrect_letters) > 0:
             print("Incorrect guesses: \n"+incorrect_letters)
 
         letter_check_correct()
         check_for_game_over()
-        
+
+    # If game_over = True, check for Player replay
     if game_over:
         check_for_replay()
+
+
+# Will only be called when you run the python program from the terminal
+if __name__ == '__main__':
+    main()
